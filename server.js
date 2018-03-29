26195799d0a37e1407d4fb4e8a4177b2f370e09f
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-29 13:56:37 -0700

    added a project-api-route.js file for project routing connected project to users through the database

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 require("./routes/user-api-routes.js")(app);
+require("./routes/project-api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
