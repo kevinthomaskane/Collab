ce3735bc152abf575bcdd5841bed0e2f63a60dec
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-29 11:23:19 -0700

    cleaning up some code

--- public/js/login.js
+++ public/js/login.js
@@ -4,34 +4,39 @@ $(document).ready(function () {
   var pass = false;
 
   $("#submitUser").on("click", () => {
-      console.log("here");
-      var userObject = {
-          name: $("#name").val().trim(),
-          username: $("#user").val().trim(),
-          password: $("#pass").val().trim()
-      }
-      if (userObject.name.length > 0 && userObject.username.length > 0 && userObject.password.length > 0) {
-          name = true;
-          user = true;
-          pass = true;
-      }
-      if (name && user && pass) {
-          $.post("/api/newUser", userObject).then((data) => {
-              if (data) {
-                  alert("Thank you for signing up, now please log in")
-              }
-          });
-      } else {
-          alert("Please fill in all fields")
-      }
-  });
+    console.log("here");
+    var userObject = {
+        name: $("#name").val().trim(),
+        username: $("#user").val().trim(),
+        password: $("#pass").val().trim()
+    }
 
+    if (userObject.name.length > 0
+      && userObject.username.length > 0
+      && userObject.password.length > 0) {
+        name = true;
+        user = true;
+        pass = true;
+    }
+
+    if (name && user && pass) {
+        $.post("/api/newUser", userObject).then((data) => {
+            if (data) {
+                alert("Thank you for signing up, now please log in")
+            }
+        });
+    } else {
+        alert("Please fill in all fields")
+    }
+
+  });
 
   $("#btn-login").on("click", () => {
     var username = $("#login-username").val().trim();
     var password = $("#login-password").val().trim();
-    
-    $.get("/api/login/"+ username + "/" + password ).then((data) => {
+
+    $.get("/api/login/"+
+      username + "/" + password ).then((data) => {
       if(data){
         console.log(data);
       }
