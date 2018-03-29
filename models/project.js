55eaab26b416550cdeef77278ef13226d4eec1fb
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-28 18:36:18 -0700

    removed a comment

--- models/project.js
+++ models/project.js
@@ -7,8 +7,6 @@ module.exports = function (sequelize, DataTypes) {
   });
 
   Project.associate = function(models) {
-    // in a many-to-many relationship, where an author can belong to many posts and vice versa, we will actually need a third table to store all of the possibilities. the "through" property will create that third table for us.
-    Project.belongsToMany(models.User, {
       through: "user2project"
     });
   };
--- models/user.js
+++ models/user.js
@@ -15,7 +15,6 @@ module.exports = function (sequelize, DataTypes) {
   });
 
   User.associate = function(models) {
-    // in a many-to-many relationship, where an author can belong to many posts and vice versa, we will actually need a third table to store all of the possibilities. the "through" property will create that third table for us.
     User.belongsToMany(models.Project, {
       through: "user2project"
     });
