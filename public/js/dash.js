d738a627e3eeafa05bdb97bb0a801c038dbb38b5
Travis Kane <travis@Traviss-MacBook-Pro.local>
2018-03-30 13:14:59 -0700

    dashjs

--- public/js/dash.js
+++ public/js/dash.js
@@ -1,44 +1,62 @@
 
-var userName = localStorage.getItem("username")
-var userId = localStorage.getItem("id")
-console.log(userId.toString());
-$(document).ready(function () {
+var userName = localStorage.getItem("username");
+var userId = localStorage.getItem("id");
 
-    var object = {
-        username: userName,
-        userId: userId.toString()
-    }
+$(document).ready(function() {
 
-    console.log(object)
+  var object = {
+    username: userName,
+    userId: userId.toString()
+  }
 
-    $.post("/api/userProjects", object).then((data) => {
-        console.log(data)
-        for (let i = 0; i < data.Projects.length; i++) {
-            $("#projectTiles").append(`
-            <button type="button" class="btn btn-primary projectButton" id="${data.Projects[i].id}">${data.Projects[i].name}</button>
+  $.post("/api/userProjects", object).then((data) => {
+    console.log(data)
+    for (let i = 0; i < data.Projects.length; i++) {
+      $("#projectTiles").append(`
+            <button type="button"
+            class="btn btn-primary projectButton"
+            data-id="${data.Projects[i].id}">
+            ${data.Projects[i].name}</button>
             `)
-            $("#projectsDrop").append("<option id='" + data.Projects[i].id + "' value='" + data.Projects[i].name + "'>")
-        }
-    })
+      $("#projectsDrop").append("<option id='" +
+        data.Projects[i].id + "' value='" +
+        data.Projects[i].name + "'>")
+    }
+  });
 
-    $(document).on("click", "option", () => {
-        var id = $(this).attr("id")
-        var object = {
-            id: id
-        }
-        $.post("/api/userProjects", object).then((data) => {
-            window.location.href = "/projectDash";
-        })
-    })
+  $("#addProj").on("click", function() {
 
-    $(document).on("click", ".projectButton", ()=>{
-        var id = $(this).attr("id")
-        var object = {
-            id: id
-        }
-        $.post("/api/userProjects", object).then((data)=>{
-            window.location.href ="/projectDash"
-        })
-    })
+    var projObj = {
+      name: "test",
+      userId: userId
+    };
+
+    $.post("/api/addProject", projObj).then((data) => {
+      console.log(data);
+    });
+
+  });
+
+  $(document).on("click", "option", () => {
+    var id = $(this).attr("id")
+    var object = {
+      id: id
+    }
+    $.post("/api/currProject", object).then((data) => {
+      window.location.href = "/projectDash";
+    });
+  });
+
+  $("#projectTiles").on("click", "button.projectButton", () => {
+    var id = $(this).attr("data-id");
+    var object = {
+      id: "1"
+    }
+    console.log($(this));
+    localStorage.setItem("project_id", "1");
+    $.post("/api/currProject", object).then((data) => {
+      window.location.href = "/projectDash"
+    });
+  });
 
 });
--- routes/project-api-routes.js
+++ routes/project-api-routes.js
@@ -1,21 +1,31 @@
 var db = require("../models");
 
-module.exports = function (app) {
-
-  app.post("/api/userProjects", function (req, res) {
-    console.log(req.body)
+module.exports = function(app) {
+  app.post("/api/userProjects", function(req, res) {
+    console.log(req.body);
     db.User.findOne({
-      include:[{
+      include: [{
         model: db.Project,
         through: {
-          attributes:[],
-          where:{
+          attributes: [],
+          where: {
             UserId: req.body.userId
           }
         }
       }]
-    }).then(function (data) {
+    }).then(function(data) {
       res.json(data);
     });
+  });
+
+  app.post("/api/addProject", function(req, res) {
+    db.Project.create({
+        name: req.body.name,
+      })
+      .then(function(project) {
+        console.log(db.Project.prototype);
+        project.setUsers([req.body.userId]);
+        res.json(project);
+      });
   });
 };
