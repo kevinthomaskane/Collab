f344e89e18c52c6ba5be8b6fdf90b23fb3e68b23
Travis Kane <travis@Traviss-MacBook-Pro.local>
2018-03-30 15:48:17 -0700

    fixed click events and added modal

--- public/js/dash.js
+++ public/js/dash.js
@@ -1,9 +1,6 @@
 var userName = localStorage.getItem("username");
 var userId = localStorage.getItem("id");
 
-var userName = localStorage.getItem("username");
-var userId = localStorage.getItem("id");
-
 $(document).ready(function() {
 
   var object = {
@@ -13,6 +10,7 @@ $(document).ready(function() {
 
   $.post("/api/userProjects", object).then((data) => {
     console.log(data)
+    $("#projectTiles").html("")
     for (let i = 0; i < data.Projects.length; i++) {
       $("#projectTiles").append(`
             <button type="button"
@@ -20,27 +18,33 @@ $(document).ready(function() {
             data-id="${data.Projects[i].id}">
             ${data.Projects[i].name}</button>
             `)
-      $("#projectsDrop").append("<option id='" +
+      $("#projectsDrop").append("<option class='options' data-id='" +
         data.Projects[i].id + "' value='" +
         data.Projects[i].name + "'>")
     }
   });
 
-  $("#addProj").on("click", function() {
-
+  $("#modalSubmit").on("click", function() {
     var projObj = {
-      name: "test",
+      name: $("#inputProject").val(),
       userId: userId
     };
-
+    $("#myModal").modal("hide")
+    $("#inputProject").val("")
     $.post("/api/addProject", projObj).then((data) => {
       console.log(data);
     });
 
   });
 
-  $(document).on("click", "option", () => {
-    var id = $(this).attr("id")
+
+  $(document).on("change", "#test", () => {
+    var id;
+    for (let i = 0; i < $(this)["0"].activeElement.list.children.length; i++){
+        if ($(this)["0"].activeElement.list.children[i].attributes[2].nodeValue === $("#test").val()){
+            id = $(this)["0"].activeElement.list.children[i].attributes[2].nodeValue
+        }
+    }
     var object = {
       id: id
     }
@@ -49,13 +53,14 @@ $(document).ready(function() {
     });
   });
 
-  $("#projectTiles").on("click", "button.projectButton", () => {
-    var id = $(this).attr("data-id");
+  $(document).on("click", ".projectButton", (event) => {
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    var id = $(this)["0"].activeElement.attributes[2].nodeValue;
     var object = {
-      id: "1"
+      id: id
     }
-    console.log($(this));
-    localStorage.setItem("project_id", "1");
+    localStorage.setItem("project_id", id);
     $.post("/api/currProject", object).then((data) => {
       window.location.href = "/projectDash"
     });
