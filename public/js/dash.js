1273ee381f90e8731f6a4382545ecccc08a93ccc
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-04-02 09:29:17 -0700

    cleaning up some code

--- public/js/dash.js
+++ public/js/dash.js
@@ -9,25 +9,28 @@ $(document).ready(function() {
     username: userName,
     userId: userId.toString()
   }
-  
+
   writeProjects();
 
-  function writeProjects(){
+  function writeProjects() {
     $.post("/api/userProjects", object).then((data) => {
-        $("#projectTiles").html("")
-        console.log(data.Projects)
-        for (let i = 0; i < data.Projects.length; i++) {
-          $("#projectTiles").append(`
-                <button type="button"
-                class="btn btn-primary projectButton">
-                <a data-id="${data.Projects[i].id}" class="projText">${data.Projects[i].name}</a>
-                <a data-toggle="modal" data-target="#deleteModal" data-id="${data.Projects[i].id}" class="delete"><i class="fas fa-times-circle"></i></a></button>
-                `)
-          $("#projectsDrop").append("<option class='options' data-id='" +
-            data.Projects[i].id + "' value='" +
-            data.Projects[i].name + "'>")
-        }
-      });  
+      $("#projectTiles").html("")
+      console.log(data.Projects)
+      for (let i = 0; i < data.Projects.length; i++) {
+        $("#projectTiles").append(`
+          <button type="button"
+          class="btn btn-primary projectButton">
+          <a data-id="${data.Projects[i].id}"
+          class="projText">${data.Projects[i].name}</a>
+          <a data-toggle="modal" data-target="#deleteModal"
+          data-id="${data.Projects[i].id}" class="delete"><i
+          class="fas fa-times-circle"></i></a></button>`);
+
+        $("#projectsDrop").append("<option class='options' data-id='" +
+          data.Projects[i].id + "' value='" +
+          data.Projects[i].name + "'>")
+      }
+    });
   }
 
   $("#modalSubmit").on("click", function() {
@@ -45,10 +48,10 @@ $(document).ready(function() {
 
   $(document).on("change", "#test", () => {
     var id;
-    for (let i = 0; i < $(this)["0"].activeElement.list.children.length; i++){
-        if ($(this)["0"].activeElement.list.children[i].attributes[2].nodeValue === $("#test").val()){
-            id = $(this)["0"].activeElement.list.children[i].attributes[2].nodeValue
-        }
+    for (let i = 0; i < $(this)["0"].activeElement.list.children.length; i++) {
+      if ($(this)["0"].activeElement.list.children[i].attributes[2].nodeValue === $("#test").val()) {
+        id = $(this)["0"].activeElement.list.children[i].attributes[2].nodeValue
+      }
     }
     var object = {
       id: id
@@ -56,44 +59,42 @@ $(document).ready(function() {
     $.post("/api/currProject", object).then((data) => {
       window.location.href = "/projectDash";
     });
-    
+
   });
 
   $(document).on("click", ".projText", (event) => {
-    var id = $(this)["0"].activeElement.children["0"].attributes["0"].nodeValue
+    var id =
+      $(this)["0"].activeElement.children["0"].attributes["0"].nodeValue;
     var object = {
       id: id
     }
     localStorage.setItem("project_id", id);
     $.post("/api/currProject", object).then((data) => {
-            window.location.href = "/projectDash"
-       
-      
+      window.location.href = "/projectDash"
+
+
     });
   });
 
-  $("#projectTiles").on("click", ".delete", ()=>{
+  $("#projectTiles").on("click", ".delete", () => {
     var id = $(this)["0"].activeElement.attributes[2].nodeValue;
-      if (deleteProject){
-        var object = {
-            id: id
-          };
-          $.ajax({
-            method: "DELETE",
-            url: "/api/userProjects",
-            data: object
-        }).then((data) => {
-            writeProjects();
-          });
-      }
-      
+    if (deleteProject) {
+      var object = {
+        id: id
+      };
+      $.ajax({
+        method: "DELETE",
+        url: "/api/userProjects",
+        data: object
+      }).then((data) => {
+        writeProjects();
+      });
+    }
+
   })
 
-    $("#deleteModal").on("click", ()=>{
-        deleteProject = true;
-    })
-  
-  
-
+  $("#deleteModal").on("click", () => {
+    deleteProject = true;
+  });
 
 });
--- public/js/projectDash.js
+++ public/js/projectDash.js
@@ -1,162 +1,169 @@
-
 var project_id = localStorage.getItem("project_id")
 
-$(document).ready(function () {
+$(document).ready(function() {
 
 
-    $.get("/api/todos/" + project_id).then((data) => {
-        for (let i = 0; i< data.length; i++){
-            $("#need").append(`
-            <li>
-                 <button data-id="${data[i].id}" type="button" class="btn btn-primary">${data[i].content}
-                     <a href="" data-id="${data[i].id}" class="need">
-                        <i class="far fa-check-circle"></i>
-                    </a>
-                     <a href="" data-id="${data[i].id}" class="todos delete">
-                        <i class="far fa-times-circle"></i>
-                    </a>
-                </button>
-             </li>
-            `)
-        }
-    })
-
-    $.get("/api/doings/" + project_id).then((data) => {
-        for (let i= 0; i< data.length; i++){
-            $("#doing").append(`
-            <li>
-                <button data-id="${data[i].id}"type="button" class="btn btn-success">${data[i].content}
-                    <a href="" data-id="${data[i].id}" class="doing">
-                            <i class="far fa-check-circle"></i>
-                    </a>
-                    <a href="" data-id="${data[i].id}" class="doings delete">
-                         <i class="far fa-times-circle"></i>
-                    </a>
-                </button>
-            </li>
+  $.get("/api/todos/" + project_id).then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      $("#need").append(`
+        <li>
+         <button data-id="${data[i].id}" type="button"
+         class="btn btn-primary">${data[i].content}
+           <a href="" data-id="${data[i].id}" class="need">
+              <i class="far fa-check-circle"></i>
+           </a>
+           <a href="" data-id="${data[i].id}" class="todos delete">
+              <i class="far fa-times-circle"></i>
+           </a>
+         </button>
+        </li>
         `)
-        }
+    }
+  })
 
-    })
-
-    $.get("/api/dones/" + project_id).then((data) => {
-        for (let i = 0; i < data.length; i++){
-            $("#done").append(`
-            <li>
-                 <button data-id="${data[i].id}" type="button" class="btn btn-dark">${data[i].content}
-                     <a href="" data-id="${data[i].id}" class="dones delete">
-                        <i class="far fa-times-circle"></i>
-                    </a>
-                </button>
-             </li>
+  $.get("/api/doings/" + project_id).then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      $("#doing").append(`
+        <li>
+         <button data-id="${data[i].id}"type="button"
+         class="btn btn-success">${data[i].content}
+          <a href="" data-id="${data[i].id}" class="doing">
+           <i class="far fa-check-circle"></i>
+          </a>
+          <a href="" data-id="${data[i].id}" class="doings delete">
+           <i class="far fa-times-circle"></i>
+          </a>
+         </button>
+        </li>
         `)
-        }
+    }
 
-    })
+  })
+
+  $.get("/api/dones/" + project_id).then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      $("#done").append(`
+        <li>
+         <button data-id="${data[i].id}" type="button"
+         class="btn btn-dark">${data[i].content}
+          <a href="" data-id="${data[i].id}" class="dones delete">
+           <i class="far fa-times-circle"></i>
+          </a>
+         </button>
+        </li>
+        `)
+    }
+
+  })
 
 
-    $(".add-need").on("click", function (event) {
-        event.preventDefault();
-        var text = $(".need-item").val().trim();
-        var todo = {
-            content: text,
-            project_id: project_id
-        }
-        $.post("/api/todos", todo).then((data) => {
-            for (let i = 0; i < data.length; i++){
-                $("#need").prepend(`
-                <li>
-                     <button data-id="${data[i].id}" type="button" class="btn btn-primary">${data[i].content}
-                         <a href="" data-id="${data[i].id}" class="need">
-                            <i class="far fa-check-circle"></i>
-                        </a>
-                         <a href="" data-id="${data[i].id}" class="todos delete">
-                            <i class="far fa-times-circle"></i>
-                        </a>
-                    </button>
-                 </li>
+  $(".add-need").on("click", function(event) {
+    event.preventDefault();
+    var text = $(".need-item").val().trim();
+    var todo = {
+      content: text,
+      project_id: project_id
+    }
+    $.post("/api/todos", todo).then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        $("#need").prepend(`
+          <li>
+           <button data-id="${data[i].id}" type="button"
+           class="btn btn-primary">${data[i].content}
+            <a href="" data-id="${data[i].id}" class="need">
+             <i class="far fa-check-circle"></i>
+            </a>
+            <a href="" data-id="${data[i].id}" class="todos delete">
+             <i class="far fa-times-circle"></i>
+            </a>
+           </button>
+          </li>
                 `)
-            }
-        });
-    })
+      }
+    });
+  })
 
-    $(document).on("click", ".need", function (event) {
-        event.preventDefault();
-        event.stopPropagation();
-        event.stopImmediatePropagation();
-        var text = $(this).parent().text().trim();
-        var id = $(this).parent().attr("data-id");
-        var todo = {
-            content: text,
-            project_id: project_id
+  $(document).on("click", ".need", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    var text = $(this).parent().text().trim();
+    var id = $(this).parent().attr("data-id");
+    var todo = {
+      content: text,
+      project_id: project_id
+    }
+    console.log(todo.project_id);
+    $(this).parent().remove();
+    $.ajax({
+      method: "DELETE",
+      url: "/api/todos/" + id
+    }).then((data) => {
+      $.post("/api/doings", todo).then((response) => {
+        for (let i = 0; i < response.length; i++) {
+          $("#doing").prepend(`
+            <li>
+             <button data-id="${response[i].id}"type="button"
+             class="btn btn-success">${response[i].content}
+                 <a href="" data-id="${response[i].id}" class="doing">
+                  <i class="far fa-check-circle"></i>
+                 </a>
+                 <a href="" data-id="${response[i].id}"
+                 class="doings delete">
+                  <i class="far fa-times-circle"></i>
+                 </a>
+             </button>
+            </li>
+                    `)
         }
-        console.log(todo.project_id);
-        $(this).parent().remove();
-        $.ajax({
-            method: "DELETE",
-            url: "/api/todos/" + id
-        }).then((data) => {
-            $.post("/api/doings", todo).then((response) => {
-                for (let i = 0; i < response.length; i++){
-                    $("#doing").prepend(`
-                    <li>
-                         <button data-id="${response[i].id}"type="button" class="btn btn-success">${response[i].content}
-                             <a href="" data-id="${response[i].id}" class="doing">
-                                <i class="far fa-check-circle"></i>
-                            </a>
-                             <a href="" data-id="${response[i].id}" class="doings delete">
-                                <i class="far fa-times-circle"></i>
-                            </a>
-                        </button>
-                     </li>
-                    `)
-                }
-            })
-        });
-    })
+      })
+    });
+  })
 
-    $(document).on("click", ".doing", function (event) {
-        event.preventDefault();
-        var text = $(this).parent().parent().text().trim();
-        var id = $(this).parent().attr("data-id")
-        var done = {
-            content: text,
-            project_id: project_id
-        };
-        $(this).parent().parent().remove();
-        $.ajax({
-            method: "DELETE",
-            url: "/api/doings/" + id,
-        }).then((data) => {
-            $.post("/api/dones", done).then((response) => {
-                for (let i = 0; i < response.length; i++){
-                    $("#done").prepend(`
+  $(document).on("click", ".doing", function(event) {
+    event.preventDefault();
+    var text = $(this).parent().parent().text().trim();
+    var id = $(this).parent().attr("data-id")
+    var done = {
+      content: text,
+      project_id: project_id
+    };
+    $(this).parent().parent().remove();
+    $.ajax({
+      method: "DELETE",
+      url: "/api/doings/" + id,
+    }).then((data) => {
+      $.post("/api/dones", done).then((response) => {
+        for (let i = 0; i < response.length; i++) {
+          $("#done").prepend(`
                     <li>
-                         <button data-id="${response[i].id}" type="button" class="btn btn-dark">${response[i].content}
-                             <a href="" data-id="${response[i].id}" class="dones delete">
-                                <i class="far fa-times-circle"></i>
-                            </a>
-                        </button>
-                     </li>
+                     <button data-id="${response[i].id}" type="button"
+                     class="btn btn-dark">${response[i].content}
+                      <a href="" data-id="${response[i].id}"
+                      class="dones delete">
+                       <i class="far fa-times-circle"></i>
+                      </a>
+                     </button>
+                    </li>
                     `)
-                }
-            })
-        });
-    })
+        }
+      })
+    });
+  })
 
-    $(document).on("click", ".delete", function (event) {
-        event.preventDefault();
-        var id = $(this).parent().attr("data-id")
-        var route = $(this).attr("class").split(" ")[0]
-        console.log(id);
-        $(this).parent().remove();
-        $.ajax({
-            method: "DELETE",
-            url: "/api/" + route + "/" + id
-        }).then((data) => {
-            console.log(data)
-        });
-    })
+  $(document).on("click", ".delete", function(event) {
+    event.preventDefault();
+    var id = $(this).parent().attr("data-id")
+    var route = $(this).attr("class").split(" ")[0]
+    console.log(id);
+    $(this).parent().remove();
+    $.ajax({
+      method: "DELETE",
+      url: "/api/" + route + "/" + id
+    }).then((data) => {
+      console.log(data)
+    });
+  })
 
 
 })
