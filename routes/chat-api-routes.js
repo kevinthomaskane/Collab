a7a052037ab6a9fa9f1c09fd0472fe3cbc7b3b32
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-04-04 12:37:32 -0700

    added a route for finding the chats

--- routes/chat-api-routes.js
+++ routes/chat-api-routes.js
@@ -1,5 +1,13 @@
 var db = require("../models");
 
 module.exports = function (app) {
-  
+  app.get("/api/chats/:id", function (req, res) {
+    db.Chat.findAll({
+      where:{
+        project_id: req.params.id
+      }
+    }).then(function (data) {
+      res.json(data);
+    })
+  });
 };
